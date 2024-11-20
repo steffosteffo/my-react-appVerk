37e0960b48a2942b78c151583dd6f2acbb5e204b
steffo <steffo@steffo.live>
2024-11-20 20:33:29 +0100

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 
 //för att koppla lokal kod till git :::::::::: git remote add origin https://github.com/steffosteffo/my-react-appVerk.git
-// för att hitta programmet på nätet:::::::::: http://steffohost-hopto.org:3000/my-react-appVerk
+// för att hitta programmet på nätet:::::::::: http://steffohost-hopto.org:3001/my-react-appVerk // DETTA FUNKAR BARA PÅ ANNAT ENHET TYP TELEFOEN 
 
 
 const App = () => {
