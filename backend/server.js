ba1b5766dd54abe04e2455ab68e6cdec8a9bd6ce
steffo <steffo@steffo.live>
2024-12-13 15:33:47 +0100

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const itemRoutes = require('./routes/items');
 
+
+
 // Load environment variables
 dotenv.config();
 
@@ -13,6 +15,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+//jjj
+//const cors = require('cors');
+//app.use(cors());
 
 // Routes
 app.use('/api/items', itemRoutes);
@@ -23,7 +28,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch((error) => console.error('Could not connect to MongoDB:', error));
 
 // Start server '0.0.0.0' to be accessible outside local network
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
